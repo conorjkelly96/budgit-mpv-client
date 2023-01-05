985dc9896eea7d4664a840f992f64b0c46ea60fe
Conor Kelly <conorjkelly96@gmail.com>
2023-01-05 11:19:57 +0000

    signup form functions

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useRef, useState } from "react";
 import { useMutation } from "@apollo/client";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
@@ -11,149 +11,78 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useForm } from "react-hook-form";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 import { useAuth } from "../contexts/AppProvider";
 import { LOGIN_USER } from "../mutations";
 import { loginForm, postButton } from "../styles";
 
 export const SignupForm = () => {
-  const { setIsLoggedIn, setUser } = useAuth();
-  const [executeLogin, { loading, error }] = useMutation(LOGIN_USER);
+  const [executeSignUp, { loading, error }] = useMutation(SIGNUP_STUDENT);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
+    control,
   } = useForm();
+
+  const password = useRef({});
+  password.current = watch("password", "");
 
   const navigate = useNavigate();
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.currentTarget);
-  //   console.log({
-  //     email: data.get("email"),
-  //     password: data.get("password"),
-  //   });
-  // };
-
-  const onSubmit = async ({ email, password }) => {
-    console.log(email, password);
-    const { data } = await executeLogin({
-      variables: {
-        input: {
-          email: email.toLowerCase().trim(),
-          password,
+  const onSubmit = async ({
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+  }) => {
+    try {
+      const { data } = await executeSignUp({
+        variables: {
+          input: {
+            firstName: firstName.toLowerCase().trim(),
+            lastName: lastName.toLowerCase().trim(),
+            username: username.toLowerCase().trim(),
+            email: email.toLowerCase().trim(),
+            password,
+          },
         },
-      },
-    });
-
-    if (data) {
-      const { token, user } = data.loginUser;
-
-      localStorage.setItem("token", token);
-      localStorage.setItem("user", JSON.stringify(user));
-
-      console.log(user);
-
-      setIsLoggedIn(true);
-      setUser({
-        id: user.id,
-        firstName: user.username,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
       });
 
-      navigate("/dashboard", { replace: true });
+      if (data) {
+        navigate("/login", { replace: true });
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
-  return (
-    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-      <Box
-        sx={{
-          my: 8,
-          mx: 4,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Log in
-        </Typography>
-        <Box
-          component="form"
-          noValidate
-          onSubmit={handleSubmit(onSubmit)}
-          sx={{ mt: 1 }}
-        >
-          <TextField
-            margin="normal"
-            id="email"
-            label="Email"
-            name="email"
-            variant="outlined"
-            fullWidth
-            {...register("email", { required: true })}
-            error={!!errors.email}
-            disabled={loading}
-          />
-          <TextField
-            type="password"
-            margin="normal"
-            id="password"
-            label="Password"
-            name="password"
-            variant="outlined"
-            fullWidth
-            {...register("password", { required: true })}
-            error={!!errors.password}
-            disabled={loading}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          {error && (
-            <Typography
-              variant="subtitle2"
-              gutterBottom
-              component="div"
-              sx={loginForm.errorContainer}
-            >
-              Failed to login, please enter valid email address and/or password.
-            </Typography>
-          )}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Log In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Grid>
-  );
+  const styles = {
+    container: {
+      backgroundColor: "#fff",
+    },
+    header: {
+      paddingTop: 2,
+      paddingBottom: 2,
+    },
+    form: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: 4,
+    },
+    loadingButton: { marginTop: 3, marginBottom: 2 },
+    errorContainer: {
+      marginTop: 2,
+      color: "#d32f2f",
+      textAlign: "center",
+    },
+  };
+
+  return <></>;
 };
